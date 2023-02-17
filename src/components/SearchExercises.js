8509c9b8220cd74918c929bf66d7855d34043540
Yurii Rybachok <drummer2009@ukr.net>
2023-02-17 19:31:04 +0200

    add similarExercises component to exercisesDetail page

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -74,6 +74,7 @@ const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
         <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
             <HorizontalScrollbar data={bodyParts} 
                 bodyPart={bodyPart} setBodyPart={setBodyPart}
+                isBodyParts
             />   
         </Box>
     </Stack>
